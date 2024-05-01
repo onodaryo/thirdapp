15da28bbed62bb6fc25fe13c936080712e35aea4
ryoonoda <onoda.ryo@genetec.co.jp>
2024-05-01 11:39:47 +0900

    カラーテーマの設定

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,42 @@ import 'package:flutter/material.dart';
 
 import 'package:keihi/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(
+    255, 160, 7, 7,),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       // theme: ThemeData(useMaterial3: true),
-      home: Expenses(),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        bottomSheetTheme: const BottomSheetThemeData(
+          modalBackgroundColor: Color.fromARGB(255, 255, 172, 182),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 14,
+          ),
+        ),
+      ),
+      home: const Expenses(),
     ),
   );
 }
