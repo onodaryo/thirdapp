8143efabe3a0d3b11cfe3233711e95bb93379934
ryoonoda <onoda.ryo@genetec.co.jp>
2024-05-01 09:50:03 +0900

    経費リストのアイテム削除機能の追加

--- lib/widgets/expensesList.dart
+++ lib/widgets/expensesList.dart
@@ -4,14 +4,22 @@ import 'package:keihi/models/expense.dart';
 import 'package:keihi/widgets/expenseItem.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses, });
+  const ExpensesList({super.key, required this.expenses, required this.onRemovedExpense});
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemovedExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: expenses.length, itemBuilder: (ctx, index) => ExpenseItem(expense: expenses[index],)
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onRemovedExpense(expenses[index]);
+        },
+        child: ExpenseItem(expense: expenses[index]),
+      ),
     );
   }
 }
